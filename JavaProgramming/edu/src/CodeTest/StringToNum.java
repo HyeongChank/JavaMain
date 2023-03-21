17a6b924737405589300d6d49c384dce095ae91a
HyeongChank <jsk381988@gmail.com>
2023-03-21 18:02:24 +0900

    update

--- JavaProgramming/edu/src/CodeTest/StringToNum.java
+++ JavaProgramming/edu/src/CodeTest/StringToNum.java
@@ -58,89 +58,151 @@ class Hash{
 public class StringToNum {
 
 	public static void main(String[] args) {
-		String numbers = "onefourzerosixseven";
+		String numbers = "onefourzerosixsevenfivefiveone";
 		long answer =0;
-		char [] ch = numbers.toCharArray();
-		add("one","1");	add("two","2"); add("three","3");
-		add("four","4");	add("five","5"); add("six","6");
-		add("seven","7");	add("eight","8"); add("nine","9");
-		add("zero","0");
-		trans(ch);
-		int [] array = new int[count];
-		int z =0;
-		while(inte != null) {
-			array[z] = inte.getRe();
-			z++;
-			inte = inte.getNext();
-		}
-		String str = "";
-		for(int i=array.length-1 ; i>=0; i--) {
-			System.out.print(array[i] +  "   ");
-			str += String.valueOf(array[i]);
-		}
-		answer = Integer.valueOf(str);
-		System.out.println(answer);
-		
-	}
-	static Hash head;
-	static Hash inte;
-	static int count=0;
 
-	public static void add(String key, String value) {
-		Hash n = head;
-		head = new Hash(key, value, n);
-	}
-	public static void addinte(int re) {
-		Hash w = inte;
-		inte = new Hash(re, w);
-		++count;
-		System.out.println("num" + re + "count" + count);
-
-	}
-//	public static void print3() {
-//		Hash p = inte;
-//		while(p != null) {
-//			System.out.print(p.getRe());
-//			p = p.getNext();
-//		}
-//	}
-//	
-//	public static void print() {
-//		Hash p = head;
-//		while(p != null) {
-//			System.out.print(p.getKey());
-//			System.out.print("  ");
-//			System.out.println(p.getValue());
-//			p = p.getNext();
-//		}
-//	}
-//	public static void printV(String key) {
-//		Hash p = head;
-//		while(p != null) {
-//			if(p.getKey()== key) {
-//				System.out.println(p.getValue());
+//		int [] key4 = {1,2,3,4,5,6,7,8,9,0};
+//		String [] key2 = {"1","2","3","4","5","6","7","8","9","0"};
+//		String [] key3 = {"one", "two", "three", "four", "five", "six","seven", "eight", "nine","zero"};
+//		for(int i =0 ; i<key2.length; i++) {
+//			if(numbers.contains(key3[i])) {
+//				numbers = numbers.replace(key3[i], key2[i]);
 //			}
-//			p = p.getNext();
+//			
 //		}
-//	}
-	public static void trans(char [] ch) {
-		Hash q = head;
-		String sc="";
+//		answer = Integer.valueOf(numbers);
+//		System.out.println(answer);
+		HashMap<String,String> hm = new HashMap<String,String>();
+		HashMap<String,String> hmc = new HashMap<String,String>();
+//		hm.put("one","1"); hm.put("two","2");
+//		hm.put("three","3"); hm.put("four","4");
+//		hm.put("five","5"); hm.put("six","6");
+//		hm.put("seven","7"); hm.put("eight","8");
+//		hm.put("nine","9"); hm.put("zero","0");
+//		
+//		// key 값 value 값 변경
+//		for(String key : hm.keySet()) {
+//			hmc.put(hm.get(key), key);
+//		}
+		hmc.put("1","one"); hmc.put("2","two");
+		hmc.put("3","three"); hmc.put("4","four");
+		hmc.put("5","five"); hmc.put("6","six");
+		hmc.put("7","seven"); hmc.put("8","eight");
+		hmc.put("9","nine"); hmc.put("0","zero");
 		
-		for(int i =0; i<ch.length; i++) {
-			q = head;
-			sc += ch[i];
-			while(q != null) {
-				if(sc.contains(q.getKey())) {
-					int b = Integer.valueOf(q.getValue());
-					System.out.print(b+ "  ");
-					addinte(b);
-					sc ="";
-			//이제 poll 만들어서 inte 에 들어간 값 빼고 최종적으로 숫자 붙이면 됨 붙일 때 문자로 하는게 나을수도 
-				}
-				q = q.getNext();
-			}			
+		for(String key : hmc.keySet()) {
+			if(numbers.contains(hmc.get(key))) {
+				numbers = numbers.replace(hmc.get(key), key);
+			}
 		}
+		answer = Integer.valueOf(numbers);
+		System.out.println("answer" + answer);
+		System.out.println(hmc);
+		System.out.println(hmc.keySet());
+//		System.out.println(hmc.values());
+//		System.out.println(hmc.containsValue("one"));
+//		for(String key : hmc.keySet()) {
+//			System.out.println(numbers.contains(hmc.get(key)));
+//			System.out.println(key + "  " + hmc.get(key));
+//			if(numbers.contains(hmc.get(key))) {
+//				hmc.get(key3).replaceAll(key, numbers)
+//			}
+//		}
+//		char [] ch2 = numbers.toCharArray();
+//		String str2 = "";
+//		numbers.contains(hm.get(str2));
+
+		
+//		char [] ch = numbers.toCharArray();
+//		System.out.println("hm" + hm);
+//		String st = "";
+//		int f =0;
+//		while(f <ch.length) {
+//			st = st + ch[f];
+//			while(hmc != null) {
+//				if(st == hmc.remove(f2))
+//			}
+//		}
+//		add("one","1");	add("two","2"); add("three","3");
+//		add("four","4");	add("five","5"); add("six","6");
+//		add("seven","7");	add("eight","8"); add("nine","9");
+//		add("zero","0");
+//		trans(ch);
+//		int [] array = new int[count];
+//		int z =0;
+//		while(inte != null) {
+//			array[z] = inte.getRe();
+//			z++;
+//			inte = inte.getNext();
+//		}
+//		String str = "";
+//		for(int i=array.length-1 ; i>=0; i--) {
+//			System.out.print(array[i] +  "   ");
+//			str += String.valueOf(array[i]);
+//		}
+//		answer = Integer.valueOf(str);
+//		System.out.println(answer);
+//		
+//	}
+//	static Hash head;
+//	static Hash inte;
+//	static int count=0;
+//
+//	public static void add(String key, String value) {
+//		Hash n = head;
+//		head = new Hash(key, value, n);
+//	}
+//	public static void addinte(int re) {
+//		Hash w = inte;
+//		inte = new Hash(re, w);
+//		++count;
+//		System.out.println("num" + re + "count" + count);
+//
+//	}
+////	public static void print3() {
+////		Hash p = inte;
+////		while(p != null) {
+////			System.out.print(p.getRe());
+////			p = p.getNext();
+////		}
+////	}
+////	
+////	public static void print() {
+////		Hash p = head;
+////		while(p != null) {
+////			System.out.print(p.getKey());
+////			System.out.print("  ");
+////			System.out.println(p.getValue());
+////			p = p.getNext();
+////		}
+////	}
+////	public static void printV(String key) {
+////		Hash p = head;
+////		while(p != null) {
+////			if(p.getKey()== key) {
+////				System.out.println(p.getValue());
+////			}
+////			p = p.getNext();
+////		}
+////	}
+//	public static void trans(char [] ch) {
+//		Hash q = head;
+//		String sc="";
+//		
+//		for(int i =0; i<ch.length; i++) {
+//			q = head;
+//			sc += ch[i];
+//			while(q != null) {
+//				if(sc.contains(q.getKey())) {
+//					int b = Integer.valueOf(q.getValue());
+//					System.out.print(b+ "  ");
+//					addinte(b);
+//					sc ="";
+//			//이제 poll 만들어서 inte 에 들어간 값 빼고 최종적으로 숫자 붙이면 됨 붙일 때 문자로 하는게 나을수도 
+//				}
+//				q = q.getNext();
+//			}			
+//		}
 
 	}
 
---
+++ JavaProgramming/sql/src/sqlconnection/dbconTest.java
@@ -0,0 +1,178 @@
+package sqlconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+
+
+public class dbconTest {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+		}
+		catch(Exception e) {
+			System.out.println("로딩");
+			e.printStackTrace();
+			return;
+		}
+		Connection conn =null;
+		PreparedStatement psmt =null;
+		PreparedStatement psmt2 =null;
+		PreparedStatement psmt3 =null;
+		PreparedStatement psmt4 =null;
+		ResultSet rs = null;
+		int result =0;
+		
+		
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bookstore","root","1234");
+
+//		select
+//			psmt = conn.prepareStatement("select * from book");
+//			psmt2 = conn.prepareStatement("select * from customer");
+//			rs = psmt2.executeQuery();
+//			while(rs.next()) {
+//			String a1 = rs.getString("name");
+//			String a2 = rs.getString("address");
+//			System.out.println(a1 + " " + a2);
+//		}			
+//
+//		insert		
+//			String query = "insert into book(bookid, bookname, publisher, price) values("
+//						+ " ?,?,?,?)";
+//			psmt3 = conn.prepareStatement(query);
+//			psmt3.setInt(1, 40);
+//			psmt3.setString(2, "갈라파");
+//			psmt3.setString(3, "good");
+//			psmt3.setInt(4, 3000);
+//			
+//			result = psmt3.executeUpdate();
+			
+//		create table	
+//			String query2 = "create table user(id varchar(20) primary key, name varchar(20), email varchar(20))";
+//			psmt4 = conn.prepareStatement(query2);
+//			result = psmt4.executeUpdate();
+
+//		insert
+//			String query = "insert into user values("
+//						+ " ?,?,?)";
+//			psmt3 = conn.prepareStatement(query);
+//			psmt3.setString(1, "jsk");
+//			psmt3.setString(2, "GP");
+//			psmt3.setString(3, "good@naver.com");
+//			result = psmt3.executeUpdate();
+//			System.out.println("완료");
+			
+			
+			Scanner kb = new Scanner(System.in);
+			System.out.println("아래 항목 중 선택하시오");
+			System.out.println("1.insert 2.select 3.show");
+			int a = kb.nextInt();
+
+			
+			switch(a) {
+			case 1 : 
+				System.out.println("테이블을 입력하시오");
+				System.out.println("table : ");
+				String e = kb.next();
+				String query = "insert into " + e +" values("
+					+ " ?,?,?)";
+				psmt3 = conn.prepareStatement(query);
+				System.out.println("insert할 정보를 입력하시오");
+				System.out.println("id : ");
+				String b = kb.next();
+				System.out.println("name : ");
+				String c = kb.next();
+				System.out.println("email : ");
+				String d = kb.next();
+				
+				psmt3.setString(1, b);
+				psmt3.setString(2, c);
+				psmt3.setString(3, d);
+				result = psmt3.executeUpdate();
+				System.out.println("insert 완료");
+			
+			case 2 :
+				System.out.println("테이블을 입력하시오");
+				System.out.println("table : ");
+				String q = kb.next();
+				System.out.println("select할 속성을 입력하시오");
+				
+				System.out.println("column : ");
+				String w = kb.next();
+				System.out.println("where 조건을 입력하시오");
+				System.out.println("where 조건문 : ");
+				String r = kb.next();
+				String query3 = "select " + w + " from " + q + " where " + r + ";";
+				psmt3 = conn.prepareStatement(query3);
+				System.out.println(query3);
+				rs = psmt3.executeQuery();
+				
+				
+				String [] str = w.split(",");
+				for(String er : str) {
+					System.out.println("str split " + er);
+				}
+				String full= "";
+				int key =0;
+				
+				
+				while(rs.next()) {
+					
+					String trt = rs.getString(str[0]);
+					String trt2 = rs.getString(str[1]);
+					System.out.println(trt + " " + trt2 + "          yeah");
+					System.out.println(rs.getString(str[0]) + " " + rs.getString(str[1]) + "             yeah2");						
+//				속성 개수만큼 출력하는 거 만들어야 함	
+
+				}
+				
+				System.out.println("Rmxdmsdlrsk");
+				
+//				System.out.println("name" + rs.getString("name"));
+//				for(int i =0; i< str.length; i++) {
+//					full += rs.getString("'" + str[i] + "'");
+//					
+//					System.out.println(full);
+//				}
+				
+			case 3 : 
+				System.out.println("테이블을 입력하시오");
+				System.out.println("table : ");
+				String g = kb.next();
+				String query2 = "select * from " + g;
+				psmt3 = conn.prepareStatement(query2);
+				rs = psmt3.executeQuery();
+				while(rs.next()) {
+					String a1 = rs.getString("id");
+					String a2 = rs.getString("name");
+					String a3 = rs.getString("email");
+					System.out.println(a1 + " " + a2 + " " + a3);
+				
+				}
+				System.out.println("show 완료");
+			}
+			
+		}
+		
+		
+		
+		
+		catch(SQLException e) {
+			System.out.println("DB 연결 오류");
+		}
+		finally {
+//			psmt.close();
+			conn.close();
+		}
+	}
+
+}
